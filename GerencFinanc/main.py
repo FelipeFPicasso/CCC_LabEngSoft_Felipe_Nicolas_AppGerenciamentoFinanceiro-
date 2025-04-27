15c5cfb97a8c1cc898f5f2119a145d210ebb371c
Felipe Picasso <felipefpicasso10@gmail.com>
2025-04-26 22:24:45 -0300

    commit_alteracao_base

--- GerencFinanc/main.py
+++ GerencFinanc/main.py
@@ -2,8 +2,12 @@ from flask import Flask
 from Controllers.usuario_controller import usuario_bp
 from Controllers.login_controller import login_bp
 from Database.migrate import validar_estrutura_db
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
 
 # Registrar os blueprints sem prefixo
 app.register_blueprint(usuario_bp)
