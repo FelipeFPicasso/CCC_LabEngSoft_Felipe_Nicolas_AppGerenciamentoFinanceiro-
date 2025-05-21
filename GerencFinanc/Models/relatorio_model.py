eab3d769448e57fb9fe60ef53a632e1be1ddfee2
Felipe Picasso <127898269+FelipeFPicasso@users.noreply.github.com>
2025-05-21 11:46:18 -0300

    commit_insercao_get_relatorio_transacao_id_usuario
    Comitando uma alteracao na rota relatorio-transacao sendo add o get utilizando o id do usuario logado pelo token

--- GerencFinanc/Models/relatorio_model.py
+++ GerencFinanc/Models/relatorio_model.py
@@ -51,3 +51,52 @@ class RelatorioTransacao:
 
         except Exception as e:
             raise Exception(f"Erro ao buscar relatório: {str(e)}")
+
+    @staticmethod
+    def buscar_por_id_usuario(id_usuario):
+        try:
+            conn = conectar_financeiro()
+            cursor = conn.cursor()
+
+            query = """
+                    SELECT r.fk_id_transacao, \
+                           t.descricao, \
+                           t.valor, \
+                           t.data, \
+                           u.nome        AS nome_usuario, \
+                           tt.tipo       AS tipo_transacao, \
+                           c.nome_banco  AS nome_conta, \
+                           cat.categoria AS nome_categoria
+                    FROM relatorio_transacao r
+                             JOIN transacao t ON t.id = r.fk_id_transacao
+                             JOIN usuario u ON t.fk_id_usuario = u.id
+                             JOIN tipo_transacao tt ON t.fk_id_tipo_transacao = tt.id
+                             JOIN conta c ON t.fk_id_conta = c.id
+                             JOIN categoria_transacao cat ON t.fk_id_categoria_transacao = cat.id
+                    WHERE t.fk_id_usuario = %s
+                    ORDER BY t.data DESC; \
+                    """
+
+            cursor.execute(query, (id_usuario,))
+            rows = cursor.fetchall()
+
+            cursor.close()
+            conn.close()
+
+            relatorios = []
+            for row in rows:
+                relatorios.append({
+                    "fk_id_transacao": row[0],
+                    "descricao": row[1],
+                    "valor": float(row[2]),
+                    "data": row[3].isoformat(),
+                    "nome_usuario": row[4],
+                    "tipo_transacao": row[5],
+                    "nome_conta": row[6],
+                    "nome_categoria": row[7]
+                })
+
+            return relatorios
+
+        except Exception as e:
+            raise Exception(f"Erro ao buscar relatórios do usuário: {str(e)}")
